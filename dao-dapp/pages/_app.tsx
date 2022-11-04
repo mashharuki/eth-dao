65a709438e78af14b24998b9816533c0e3e2b194
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-11-04 22:06:20 +0900

    デフォルトのコンポーネントを更新

--- dao-dapp/pages/_app.tsx
+++ dao-dapp/pages/_app.tsx
@@ -1,13 +1,20 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import HeadComponent from '../components/head';
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Goerli;
 
+/**
+ * MyApp
+ * @param param0 
+ * @returns 
+ */
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <HeadComponent/>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- dao-dapp/pages/index.tsx
+++ dao-dapp/pages/index.tsx
@@ -7,45 +7,11 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
+          Welcome to Mash DAO!!🚀
         </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
+        <p></p>
         <div className={styles.connect}>
           <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
         </div>
       </main>
     </div>
