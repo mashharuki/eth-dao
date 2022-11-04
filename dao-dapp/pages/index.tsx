93a9563e23c7fdaa257e7212eb925d6d03af8589
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-11-04 22:15:46 +0900

    レッスン1完了

--- dao-dapp/pages/index.tsx
+++ dao-dapp/pages/index.tsx
@@ -1,20 +1,46 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { 
+  ConnectWallet,
+  ChainId,
+  useNetwork,
+  useAddress
+} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
+/**
+ * Home Component
+ * @returns 
+ */
 const Home: NextPage = () => {
+  // get address
+  const address = useAddress();
+  // get network ID
+  const [network, switchNetwork] = useNetwork();
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Mash DAO!!🚀
-        </h1>
-        <p></p>
-        <div className={styles.connect}>
-          <ConnectWallet />
+    <>
+      {(address && network && network?.data?.chain?.id !== ChainId.Goerli)?(
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <h1 className={styles.title}>Goerli に切り替えてください⚠️</h1>
+            <p>この dApp は Goerli テストネットのみで動作します。</p>
+            <p>ウォレットから接続中のネットワークを切り替えてください。</p>
+          </main>
         </div>
-      </main>
-    </div>
+      ):(
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <h1 className={styles.title}>
+              Welcome to Mash DAO!!🚀
+            </h1>
+            <p></p>
+            <div className={styles.connect}>
+              <ConnectWallet />
+            </div>
+          </main>
+        </div>
+      )}
+    </>
   );
 };
 
